c9b2d062e4813c712c77d1feadc64affca213bd3
k-guest <k.guest.fr@gmail.com>
2023-01-19 12:07:47 +0100

    Initialization

---
+++ src/components/App/index.js
@@ -0,0 +1,11 @@
+import './style.scss';
+
+function App () {
+    return (
+        <div className="app">
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/ReactApp/index.js
@@ -0,0 +1,12 @@
+import './style.scss';
+
+function ReactApp () {
+    return (
+        <div className="react-app">
+            <img className="react-app-picture" src='react.png'></img>
+            <div className="react-app-text">React App</div>
+        </div>
+    );
+}
+
+export default ReactApp;
