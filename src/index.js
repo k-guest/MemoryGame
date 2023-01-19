6d7d168950e75f00545a7063b069b6b5f1e97659
k-guest <k.guest.fr@gmail.com>
2023-01-19 12:11:57 +0100

    Update App Component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import '../src/styles/style.scss';
 
 import { createRoot } from 'react-dom/client';
 
-// import App from './components/App';
+import App from './components/App';
 
-// const rootReactElement = <App />;
-
-import ReactApp from './components/ReactApp';
-
-const rootReactElement = <ReactApp />;
+const rootReactElement = <App />;
 
 const root = createRoot(document.getElementById('root'));
 
